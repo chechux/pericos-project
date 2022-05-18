dd4cf6e01220ad5bf4c1cf39bcfd4682cff0c435
Jesús Nieto <jnietoribera@gmail.com>
2022-05-18 13:36:03 +0200

    añadimos sonido y terminamos dimension

--- chechu/dimension_fondo_de_bikini.py
+++ chechu/dimension_fondo_de_bikini.py
@@ -1,3 +1,4 @@
+import clases as cl,sonidos_pantallas as sp
 import time
 import random
 ###Dimension Fondo de bikini###
@@ -5,18 +6,18 @@ import random
 ##items
 
 #sk
-savia_gary=("SaviGary","sk",0,"tirale savia de gary a tu rival para que no pueda atacar durante un turno!",1)
+savia_gary=("SaviGary","sk",0,"Tirale savia de gary a tu rival para que no pueda atacar durante un turno!",1)
 
 #ataque
 caza_medusas = Items("CazaMedusas 3000", "a", random.randrange(10,25),"Con el nuevo modelo CazaMedusas aumenta tu daño!",1)
-espatula = Items("Espatula de Oro","a",random.randrange(20,25),"el arma legendaria de Bobesponja! calcina a tus rivales.",1)#legendaria
+espatula = Items("Espatula de Oro","a",random.randrange(20,25),"El arma legendaria de Bobesponja! calcina a tus rivales.",1)#legendaria
 
 #sanacion
-bote_burbujas = Items("Burbujeitor","s",random.randrange(10,25),"rociate de este jabon burbujeante para recuperar salud!",1)
-cangreburguer = Items("BurguerCangreburguer","s",random.randrange(20,25), "comete esta delicia para quedarte como nuevo!",1)#legendaria
+bote_burbujas = Items("Burbujeitor","s",random.randrange(10,25),"Rociate de este jabon burbujeante para recuperar salud!",1)
+cangreburguer = Items("BurguerCangreburguer","s",random.randrange(20,25), "Cómete esta delicia para quedarte como nuevo!",1)#legendaria
 
 #precision
-gafas = Items("Lectometor","p",30,"ponte estas gafas de lectura para aumentar tu precision",1)
+gafas = Items("Lectometor","p",30,"Ponte estas gafas de lectura para aumentar tu precision",1)
 
 
 
@@ -29,12 +30,10 @@ calamardo=Mob("Calamardo",100,5,5,50,10)
 
 ##habilidades
 
-comida_cubo = Habilidades("Comida Podrida","veneno",(5,10),"envenena a tu enemigo con comida del cubo de cebo, ¡puaaaaaj!")
+golpe_cubo = Habilidades("Comida Podrida","veneno",(5,10),"Envenena a tu enemigo con comida del cubo de cebo, ¡puaaaaaj!")
 golpe_trucado = Habilidades("¿Hay trampa?","normal",(0,50),"Ha bobesponja le ha crecido el brazo sorprendentemente, pero... no parece muy estable")
-golpe_ancla = Habilidades("golpe ancla","normal",(10,20),"¡Lenate de valor y pegale una buena a tu rival!")
-
-
-                          
+golpe_ancla = Habilidades("Golpe ancla","normal",(10,20),"¡Lenate de valor y zúrrale una buena a tu rival!")
+golpe_condimento = Habilidades("Golpe condimento","normal",(10,20), "Disparale a tu oponente pero, con mucha salsa")
 
 
 ##dimension
@@ -49,31 +48,36 @@ fdb = [dimension_fondo_de_bikini]
  ##jugador                   
 
 
-bob = Jugador("Bobesponja",100,30,80,80,20,"¡Este jugador es muy escurridizo y rápido!",[comida_cubo, golpe_trucado, golpe_ancla,])
+bob = Jugador("Bobesponja",100,30,80,80,20,"¡Este jugador es muy escurridizo, rápido y el mejor cocinero de todas las dimensiones!","./recursos/risa.mp3",
+              [comida_cubo, golpe_trucado, golpe_ancla, golpe_condimento])
 
 def mostrar_bob():
     print("""
 
 ---------------------------------------------------------------------------------------------
 
-        nombre: {}                                                                                    
+        Nombre: {}                                                                                    
                                                                                             .--..--..--..--..--..--.                                                                     
-        ataque : {}                                                                  |(_.'  |    /    .-\-.  \   |
+        Ataque : {}                                                                  |(_.'  |    /    .-\-.  \   |
                                                                                          \     0|    |   ( O| O)   |                                                                                                            
-        defensa : {}                                                                  \ (_) | o         -` .-`   |
+        Defensa : {}                                                                  \ (_) | o         -` .-`   |
                                                                                              |    \   |`-._ _ _ _ _ \ /
-        precision : {}                                                                  \    |   |  `. |_||_|     |
+        Precision : {}                                                                  \    |   |  `. |_||_|     |
                                                                                               | o  |    \_      \      |       
-        velocidad : {}                                                                  |     \     `--..-'   O |  
+        Velocidad : {}                                                                  |     \     `--..-'   O |  
                                                                                                     |     `-.-'      /-.__         
-        descripcion : {}
+        Descripcion : {}
         
 ---------------------------------------------------------------------------------------------
 
+""". format(bob.nombre, bob.ataque, bob.defensa, bob.precision, bob.velocidad, bob.descripcion))
 
 
+mihilo_mostrar_bob = sp.Songhilo_jugadores()
+mihilo_mostrar_bob.set_song(bob.sonido)
+mihilo_mostrar_bob.start()
+del mihilo_mostrar_bob
 
-""")
 mostrar_bob()
-    
+
 
